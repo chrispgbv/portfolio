790302f8100d73a17567d0a71a5b73e27a81aa76
Christine Garcia Benito Valkner <35257362+chrispgbv@users.noreply.github.com>
2021-06-06 21:49:44 +0200

    started portfolio slideshow

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,9 +1,29 @@
 import "./works.scss"
+import arrow from "./arrow.png"
 
 export default function Works() {
     return (
         <div className = "work" id ="work"> 
-            
+        <div className="slider">
+        <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                  </div>
+                  <h2> Title </h2>
+                  <p> I wrote my bachelors on creating a javascript library out by using canvas to create heart compressions for Laerdal Medicals manikins. </p>
+                  <span>https://github.com/chrispgbv/haart</span>
+                </div>
+              </div>
+              <div className="right">
+              <img src="https://i.pinimg.com/564x/b6/97/61/b69761529fa0790d0a308b756e00b1e6.jpg" alt="" />
+              </div>
+            </div>
+          </div>
+          </div>
+            <img src={arrow} className ="arrow left" alt="arrow" />
+            <img src={arrow} className ="arrow right" alt="arrow" />
         </div>
     )
 }
